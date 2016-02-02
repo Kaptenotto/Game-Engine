2c4c429a9b896185b1592bb62dca9a65af219914
murbruksprodukt <johan.ganeteg@gmail.com>
2016-02-02 15:09:39 +0100

    Fragment är johansa pixelshader

--- Fragment.hlsl
+++ Fragment.hlsl
@@ -1,17 +1,47 @@
-Texture2D txDiffuse : register (t0);
-SamplerState sampAni;
+Texture2D shaderTexture : register(t0);
+Texture2D depthMapTexture : register(t1);
+SamplerState SampleTypeClamp : register(s0);
+SamplerState SampleTypeWrap  : register(s1);
 
-struct VS_OUT
+cbuffer LightBuffer
 {
+	float4 ambientColor;
+	float4 diffuseColor;
+};
+struct PS_IN
+{
+	float4 position : SV_POSITION;
+	float2 tex : TEXCOORD0;
+	float3 normal : NORMAL;
+	float3 tangent : TANGENT;
+	float4 lightViewPosition : TEXCOORD1;
+	float3 lightPos : TEXCOORD2;
+};
+
+struct PS_OUT
+{
+	float bias;
 	float4 pos : SV_POSITION;
 	float3 Color : COLOR;
 	float3 normal : NORMAL;
 };
 
-float4 PS_main(VS_OUT input) : SV_Target
+
+float4 PS_main(PS_OUT input) : SV_Target
 {
+	float bias;
+	float4 color;
+	float2 projectTexCoord;
+	float depthValue;
+	float lightDepthValue;
+	float lightIntensity;
+	float4 textureColor;
+
+	bias = 0.001f;
+	color = ambientColor;
+	projectTexCoord.x = input.lightViewPosition.x / input.lightViewPosition.w / 2.0f + 0.5f;
+	projectTexCoord.y = -input.lightViewPosition.y / input.lightViewPosition.w / 2.0f + 0.5f;
 	
-	float3 s = txDiffuse.Sample(sampAni, input.Color).xyz;
 
 	return float4(input.Color, 1.0f);
 };
