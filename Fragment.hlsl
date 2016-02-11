095225d0bec0b0a30a7bb2b164ae73ba9fd29350
murbruksprodukt <johan.ganeteg@gmail.com>
2016-02-11 14:21:25 +0100

    försöker få fram kuben igen

--- Fragment.hlsl
+++ Fragment.hlsl
@@ -1,61 +1,70 @@
 Texture2D txDiffuse : register (t0);
-Texture2D depthMapTexture : register(t1);
-SamplerState SampleTypeClamp : register(s0);
-SamplerState SampleTypeWrap  : register(s1);
+//Texture2D depthMapTexture : register(t1);
+//SamplerState SampleTypeClamp : register(s0)
+//{
+//	Filter = MIN_MAG_MIP_POINT;
+//	AddressU = Clamp;
+//	AddressV = Clamp;
+//};
+//SamplerState SampleTypeWrap  : register(s0);
+SamplerState sampAni;
 
 struct VS_OUT
 {
 	float3 pos : POSITION;
-	float3 norm : TEXCOORD1;
-	float2 tex : COLOR; //ska va tex
-	float4 posLightH : TEXCOORD3;
+	float3 norm : NORMAL;
+	float3 tex : COLOR; //ska va tex
+//	float4 posLightH : TEXCOORD3;
 };
-struct Light
-{
-	float4 ambient;
-	float4 diffuse;
-};
+//struct Light
+//{
+//	float4 ambient;
+//	float4 diffuse;
+//};
 
 float4 PS_main(VS_OUT input) : SV_Target
 {
-	Light light = (Light)0;
+	//Light light = (Light)0;
 
-	float bias;
-	float4 color;
-	float2 projectTexCoord;
-	float depthValue;
-	float lightDepthValue;
-	float lightIntensity;
-	float4 textureColor;
+	//float bias;
+	//float4 color;
+	//float2 projectTexCoord;
+	//float depthValue;
+	//float lightDepthValue;
+	//float lightIntensity;
+	//float4 textureColor;
 
-	bias = 0.001f;
-	color = light.ambient;
-	projectTexCoord.x = input.posLightH.x / input.posLightH.w / 2.0f + 0.5f;
-	projectTexCoord.y = -input.posLightH.y / input.posLightH.w / 2.0f + 0.5f;
+	//bias = 0.001f;
+	////color = light.ambient;
+	//light.diffuse = float4(input.tex, 1.0f);
+	//light.ambient = float4(0.2f, 0.2f, 0.2f, 2.0f);
+	//color = light.ambient;
+	//projectTexCoord.x = input.posLightH.x / input.posLightH.w / 2.0f + 0.5f;
+	//projectTexCoord.y = -input.posLightH.y / input.posLightH.w / 2.0f + 0.5f;
 
-	if ((saturate(projectTexCoord.x) == projectTexCoord.x) && (saturate(projectTexCoord.y) == projectTexCoord.y))
-	{
-		depthValue = depthMapTexture.Sample(SampleTypeClamp, projectTexCoord.r);
-		lightDepthValue = input.posLightH.z / input.posLightH.w;
-		lightDepthValue = lightDepthValue - bias;
+	//if ((saturate(projectTexCoord.x) == projectTexCoord.x) && (saturate(projectTexCoord.y) == projectTexCoord.y))
+	//{
+	//	depthValue = depthMapTexture.Sample(SampleTypeClamp, projectTexCoord.r);
+	//	lightDepthValue = input.posLightH.z / input.posLightH.w;
+	//	lightDepthValue = lightDepthValue - bias;
 
-		if (lightDepthValue < depthValue)
-		{
-			lightIntensity = saturate(dot(input.norm, input.posLightH)); //kan behöva ny tex (lightpos sepparat från poslightH
-			if (lightIntensity > 0.0f)
-			{
-				color += (light.diffuse * lightIntensity);
-				color = saturate(color);
-			}
-		}
-	}
+	//	if (lightDepthValue < depthValue)
+	//	{
+	//		lightIntensity = saturate(dot(input.norm, input.posLightH)); //kan behöva ny tex (lightpos sepparat från poslightH
+	//		if (lightIntensity > 0.0f)
+	//		{
+	//			color += (light.diffuse * lightIntensity);
+	//			color = saturate(color);
+	//		}
+	//	}
+	//}
 
-	textureColor = txDiffuse.Sample(SampleTypeWrap, input.tex);
-	color = color * textureColor;
+	//textureColor = txDiffuse.Sample(SampleTypeWrap, input.tex);
+	//color = color * textureColor;
 
-	return color;
+	//return color;
 
-	//float3 s = txDiffuse.Sample(sampAni, input.Color).xyz;
+	float3 s = txDiffuse.Sample(sampAni, input.tex).xyz;
 
-	//return float4(input.Color, 1.0f);
+	return float4(input.tex, 1.0f);
 };
--- Vertex.hlsl
+++ Vertex.hlsl
@@ -1,41 +1,41 @@
-
-matrix gLightWVP;
-
-//shadowdepthmapstuff
-float4 VS(float3 posLight : POSITION) : SV_POSITION
-{
-	return mul(float4(posLight, 1.0f), gLightWVP);
-};
-
-BlendState NoBlend
-{
-	BlendEnable[0] = FALSE;
-};
-
-RasterizerState rs
-{
-	FillMode = Solid;
-	CullMode = Front;
-};
-
-DepthStencilState EnableDepth
-{
-	DepthEnable = TRUE;
-	DepthWriteMask = ALL;
-	DepthFunc = LESS_EQUAL;
-};
-technique10 RenderShadowMap
-{
-	pass P0
-	{
-		SetVertexShader(CompileShader(vs_4_0, VS()));
-		SetGeometryShader(NULL);
-		SetPixelShader(NULL);
-		SetDepthStencilState(EnableDepth, 0);
-		SetBlendState(NoBlend, float4(0.0f, 0.0f, 0.0f, 0.0f), 0xFFFFFFFF);
-		SetRasterizerState(rs);
-	}
-};
+//
+//matrix gLightWVP;
+//
+////shadowdepthmapstuff
+//float4 VS(float3 posLight : POSITION) : SV_POSITION
+//{
+//	return mul(float4(posLight, 1.0f), gLightWVP);
+//};
+//
+//BlendState NoBlend
+//{
+//	BlendEnable[0] = FALSE;
+//};
+//
+//RasterizerState rs
+//{
+//	FillMode = Solid;
+//	CullMode = Front;
+//};
+//
+//DepthStencilState EnableDepth
+//{
+//	DepthEnable = TRUE;
+//	DepthWriteMask = ALL;
+//	DepthFunc = LESS_EQUAL;
+//};
+//technique10 RenderShadowMap
+//{
+//	pass P0
+//	{
+//		SetVertexShader(CompileShader(vs_4_0, VS()));
+//		SetGeometryShader(NULL);
+//		SetPixelShader(NULL);
+//		SetDepthStencilState(EnableDepth, 0);
+//		SetBlendState(NoBlend, float4(0.0f, 0.0f, 0.0f, 0.0f), 0xFFFFFFFF);
+//		SetRasterizerState(rs);
+//	}
+//};
 
 cbuffer MatrixBuffer : register (b0)
 {
@@ -48,15 +48,15 @@ struct VS_IN
 {
 	float4 pos : POSITION;
 	float3 norm : NORMAL;
-	float2 tex : COLOR;
+	float3 tex : COLOR;
 };
 
 struct VS_OUT
 {
 	float4 pos : POSITION;
-	float3 norm : TEXCOORD1;
-	float2 tex : COLOR;
-	float4 posLightH : TEXCOORD3;
+	float3 norm : NORMAL;
+	float3 tex : COLOR;
+	//float4 posLightH : TEXCOORD3;
 };
 
 //-----------------------------------------------------------------------------------------
@@ -71,7 +71,7 @@ VS_OUT VS_main(VS_IN input)
 	output.pos = mul(input.pos, cMatrix);
 	output.norm = mul(input.norm, worldMatrix);
 	output.tex = input.tex;
-	output.posLightH = mul(input.pos, gLightWVP);
+	//output.posLightH = mul(input.pos, gLightWVP);
 
 	return output;
 }
